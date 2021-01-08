58d02854e92546d6256a56ca13437f5e0f53a07e
yuriy <yuri.zagorulko@clockwise.software>
2021-01-08 23:18:11 +0200

    setup image files serving

--- backend/marketBackend/apps/market/models/product.py
+++ backend/marketBackend/apps/market/models/product.py
@@ -6,12 +6,9 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField(max_length=2000, help_text="This is the description of the product")
     images = models.OneToOneField(ImageAlbum, related_name='model', on_delete=models.CASCADE)
+    # barcode = models.CharField(max_length=150)
+    # productCode = models.CharField(max_length=150)
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # def save(self, *args, **kwargs):
-    #
-    #     if self.pk is None:
-    #         small=rescale_image(self.image,width=100,height=100)
-    #         self.image_small=SimpleUploadedFile(name,small_pic)
-    #     super().save(*args, **kwargs)
--- backend/marketBackend/urls.py
+++ backend/marketBackend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import settings
 
 urlpatterns = [
     path('market/', include('marketBackend.apps.market.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
