0d2d02e6e567a510a52909534b63a9f6e714eaec
Rostyslav Zahorulko <balaclubb@gmail.com>
2022-10-30 10:50:43 +0200

    added img preview

--- front-end/components/shared/customImg/customImg.tsx
+++ front-end/components/shared/customImg/customImg.tsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { defaultProductImg } from '../../../helpers/constants/urls'
+import imgPreview from '../../../public/images/whileImgLoadPREVIEW.jpeg'
+
 
 type ImgProps = {
   img: string
   previev?: string,
   imgProps?: any,
   alt?: string,
-  layout?: string
+  layout?: string,
 }
 
 function CustomImg (props: ImgProps){
   const [state, setState] = useState({
     isImageError: false,
-    displayImg: defaultProductImg
+    isShowPreview: true,
+    displayImg: defaultProductImg,
   })
   useEffect(() => {
     if (!state.isImageError) {
       if (props.img) {
         setState({
           ...state,
-          displayImg: props.img
+          displayImg: state.isShowPreview ? imgPreview.src : props.img 
         })
       }
     } else {
@@ -29,11 +32,17 @@ function CustomImg (props: ImgProps){
         displayImg: props.previev ? props.previev : defaultProductImg
       })
     }
-  }, [props.img, props.previev, state.isImageError])
+  }, [props.img, props.previev, state.isImageError,state.isShowPreview])
   const imageErrorHandler = (err) => {
     setState({
       ...state,
       isImageError: true
+    })
+  }
+  const productImgLoadHandler = ()=>{
+    setState({
+      ...state,
+      isShowPreview:false
     })
   }
   return (
@@ -44,6 +53,7 @@ function CustomImg (props: ImgProps){
       objectFit='contain'
       { ...props.imgProps }
       onError={imageErrorHandler}
+      onLoad={productImgLoadHandler}
     />
   )
 }
